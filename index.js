95398a0a7a8f1248c8bf230b794ffc8949f3b86d
trunglen <trunglenlvn@gmail.com>
2018-07-25 01:02:17 +0700

    ok

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ const app = express();
 app.use(bodyParser.json());
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use('/users', userApi);
 app.use('/link', linkApi);
 app.use('/posts', postApi);
